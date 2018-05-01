4f9071a54a145bd1e823b7a64617b605dbaa31fd
Erichain <erichainzain@gmail.com>
2018-05-01 10:41:46 +0800

    Put data to database

--- index.js
+++ index.js
@@ -2,27 +2,48 @@ const Koa = require('koa');
 const axios = require('axios');
 const fs = require('fs');
 const shell = require('shelljs');
+const dateFormat = require('dateformat');
 
 const app = new Koa();
 
-fs.open('./static/data', 'wx', (error, fd) => {
-  if (error.code === 'EEXIST') {
-    console.log('Directory already exists');
-  } else {
-    shell.mkdir('-p', './static/data');
-  }
+function formatDate() {
+  return dateFormat(new Date(), 'yyyy-mm-dd_HH-MM-ss');
+}
 
-  writeDataToFile(fd);
-});
+function beginLog() {
+  fs.open('./static/data', 'wx', (error) => {
+    if (error.code === 'EEXIST') {
+      console.log('Directory already exists');
+    } else {
+      shell.mkdir('-p', './static/data');
+    }
 
-function writeDataToFile(fd) {
-  axios.get('http://localhost:9200').then((data) => {
-    console.log(data.data);
+    writeDataToFile();
+  });
+}
 
-    fs.writeFile(`./static/data/data-${new Date()}.json`, JSON.stringify(data.data, null, 2), null, () => {
-      console.log('success');
+function putDataToDB() {
+  axios.put('http://localhost:9200/megacorp/employee/1', {
+    "first_name" : "John",
+    "last_name" :  "Smith",
+    "age" :        25,
+    "about" :      "I love to go rock climbing",
+    "interests": [ "sports", "music" ]
+  });
+}
+
+function writeDataToFile() {
+  const filePath = `./static/data/log-${formatDate()}.json`;
+
+  axios.get('http://localhost:9200/_search').then((data) => {
+    fs.writeFile(filePath, JSON.stringify(data.data, null, 2), null, () => {
+      console.log('File writing success!');
     });
   });
 }
 
+putDataToDB();
+beginLog();
+
 app.listen(8881);
+console.log('Server is listening at localhost:8881');
